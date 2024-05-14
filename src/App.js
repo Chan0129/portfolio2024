3754e9885f73de5c61dcb5a5954fcb5afa6fa9af
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-05-14 11:45:30 +0800

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import Home from "./containers/home";
+import About from "./containers/about";
+import Resume from "./containers/resume";
+import Skills from "./containers/skills";
+import Contact from "./containers/contact";
+import Navbar from "./components/navBar";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* particles js */}
+
+      {/* navbar */}
+      <Navbar />
+
+      {/* main page content */}
+      <div className="App__main-page-content">
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/pageHeaderContent/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.scss";
+
+const PageHeaderContent = (props) => {
+  const { headerText, icon } = props;
+
+  return (
+    <div className="wrapper">
+      <h2>{headerText}</h2>
+      <span>{icon}</span>
+    </div>
+  );
+};
+
+export default PageHeaderContent;
